8138ee1e2b5362d0340747135096ed603fd4eb74
amanuel <amantesfazgi19@gmail.com>
2019-03-25 07:43:29 -0400

    initial commit with camera params

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/amanuel/catkin_ws_proj/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/amanuel/catkin_ws_proj/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/amanuel/catkin_ws_proj/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/amanuel/catkin_ws_proj/src:$ROS_PACKAGE_PATH"
