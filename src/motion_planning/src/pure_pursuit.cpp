6eb5b875ba9e417a42e58705f1095ee5940a2aaa
amanuel <amantesfazgi19@gmail.com>
2019-04-08 00:06:23 -0400

    changed control mechanism

--- src/motion_planning/src/pure_pursuit.cpp
+++ src/motion_planning/src/pure_pursuit.cpp
@@ -99,6 +99,9 @@ class PurePursuit{
         double robotPosY(){
             return odom_input_data.pose.pose.position.y;
         }
+        geometry_msgs::Pose currentRobotPose(){
+            return odom_input_data.pose.pose;
+        }
         double getEuclideanDistance(double x1, double y1, double x2, double y2){
             return sqrt(pow(x2-x1, 2) + pow(y2-y1, 2));
         }
@@ -174,6 +177,20 @@ class PurePursuit{
                 return false;
             }
         }
+        geometry_msgs::Point getRelativeCoordinate(geometry_msgs::Point point_msg, geometry_msgs::Pose current_pose)
+        {
+            tf::Transform inverse;
+            tf::poseMsgToTF(current_pose, inverse);
+            tf::Transform transform = inverse.inverse();
+
+            tf::Point p;
+            pointMsgToTF(point_msg, p);
+            tf::Point tf_p = transform * p;
+            geometry_msgs::Point tf_point_msg;
+            pointTFToMsg(tf_p, tf_point_msg);
+
+            return tf_point_msg;
+        }
         /**
         WayPoint* get_closest_path_point(){
             double smallest = get_euclidean_distance(robotPosX(), robotPosY(), figure8_path[0]->x, figure8_path[0]->y);
@@ -218,8 +235,8 @@ class PurePursuit{
                         *last_visited_waypt_idx = i;
                         *next_waypt_idx = i+1;
                         double slope = (waypt_next->y - waypt_i->y) / (waypt_next->x - waypt_i->x);
-                        double x_m = waypt_i->x + sign(waypt_next->x - waypt_i->x) * look_ahead_dist * sqrt(1/(1+pow(slope, 2)));
-                        double y_m = waypt_i->y + sign(waypt_next->y - waypt_i->y) * look_ahead_dist * slope * sqrt(1/(1+pow(slope, 2)));
+                        double x_m = current_pose->x + sign(waypt_next->x - waypt_i->x) * look_ahead_dist * sqrt(1/(1+pow(slope, 2)));
+                        double y_m = current_pose->y + sign(waypt_next->y - waypt_i->y) * look_ahead_dist * slope * sqrt(1/(1+pow(slope, 2)));
                         WayPoint* interpolated_waypt = new WayPoint(x_m, y_m, 0.0);
                         return interpolated_waypt;
                     } else {
@@ -244,17 +261,26 @@ class PurePursuit{
 
         }
 
-        double getDesiredCurvature(WayPoint* goalpt){
-            double curvature = 2*(goalpt->x - robotPosX())/ pow(look_ahead_dist, 2);
-            return curvature;
-        }
-        double getCurrentCurvature(WayPoint* goalpt){
+        // double getDesiredCurvature(WayPoint* goalpt){
+        //     double curvature = 2*(goalpt->x - robotPosX())/ pow(look_ahead_dist, 2);
+        //     return curvature;
+        // }
+        double getCurvature(WayPoint* goalpt){
+            double curvature = 0;
+            geometry_msgs::Point goal_point;
+            goal_point.x = goalpt->x;
+            goal_point.y = goalpt->y;
+            goal_point.z = 0;
+            geometry_msgs::Point tf_goal_point = getRelativeCoordinate(goal_point, currentRobotPose());
             double dist = getEuclideanDistance(robotPosX(), robotPosY(), goalpt->x, goalpt->y);
-            return 2*(goalpt->x - robotPosX())/ pow(dist, 2);
+            if(dist != 0){
+                curvature = 2*(tf_goal_point.y)/ pow(dist, 2);
+            } 
+            return curvature;
         }
 
         void basic_pursuit(double tolerance){
-            Rate my_rate(10);
+            
             geometry_msgs::Twist twist_msg;
             double forward_speed = 0.4;
             twist_msg.linear.x = forward_speed;
@@ -263,115 +289,58 @@ class PurePursuit{
 
             twist_msg.angular.x = 0;
             twist_msg.angular.y = 0;
-            
+            int k = 0;
             spinOnce();
+            Rate my_rate(3);
             while(!pathFinished()){
-                if(onPath()){
-                    cout<<"okay here"<<endl;
+                //if(onPath()){
+                if(1){
+                    cout<<"**** "<< k <<" ****"<<endl; 
                     WayPoint* goalpt = getGoalptLookAheadDistanceAway();
-                    cout<<"good here"<<endl;
-                    double desired_curvature = getDesiredCurvature(goalpt);
-                    cout<<"okay 2"<<endl;
-                    Rate my_rate2(100);
-                    while(getEuclideanDistance(robotPosX(), robotPosY(), goalpt->x, goalpt->y > 0.001)){
-                        double current_curvature = getCurrentCurvature(goalpt);
-                        double integral_curv = 0.0, derivative_curv = 0.0, lastError_curv = 0.0;
-                        double Kp = 2/pow(look_ahead_dist,2);
-                        double Ki = 0.01;
-                        double Kd = 0.06;
+                    last_goalpt = goalpt;
+                    double desired_curvature = getCurvature(goalpt);
+                    // double integral_curv = 0.0, derivative_curv = 0.0, lastError_curv = 0.0;
+                    // double Kp = 2/pow(look_ahead_dist,2);
+                    // double Ki = 0.01;
+                    // double Kd = 0.06;
+                        cout<< "    Goal point: "<<goalpt -> x << ","<<goalpt-> y<<endl;
+                        cout<<"     dist_from_goal: "<<getEuclideanDistance(robotPosX(), robotPosY(), goalpt->x, goalpt->y)<<endl;
+                        cout<<"     robot_pose: "<<robotPosX()<<","<<robotPosY()<<endl;
 
-                        // PD control for steering;
-                        double error = goalpt->x - robotPosX();
-                        integral_curv = integral_curv + error;
-                        derivative_curv = error - lastError_curv;
-                        lastError_curv = error;
-                        // double turn = Kp*error_yaw + Ki*integral_yaw + Kd*derivative_yaw;
-                        double turn = Kp*error + Kd*derivative_curv;
-                        twist_msg.angular.z = turn;
-                        vel_pub.publish(twist_msg);
-                        spinOnce();
-                        my_rate2.sleep();
-                    }
+                    // double error = goalpt->y - robotPosY();
+                    // integral_curv = integral_curv + error;
+                    // derivative_curv = error - lastError_curv;
+                    // lastError_curv = error;
+                    // double turn = Kp*error_yaw + Ki*integral_yaw + Kd*derivative_yaw;
+                    double turn = desired_curvature*forward_speed;
+                    cout<<"       turn: "<< turn<< endl;
+                   
+                    twist_msg.angular.z = turn;
+                    vel_pub.publish(twist_msg);
+                    spinOnce();  
+                    //cout<< "Goal point: "<<goalpt -> x << ","<<goalpt-> y<<endl;
+                    //cout<< "curvature: "<<desired_curvature<<endl;
                     
+               
+                    cout<< "desired_curvature: "<<desired_curvature<<endl;
+                    cout<<"radius: "<<1/desired_curvature<<endl;
+                    cout<<"euclidean dist: "<<getEuclideanDistance(robotPosX(), robotPosY(), goalpt->x, goalpt->y)<<endl;
+                    cout<<"lookahead: "<<look_ahead_dist<<endl;
+                    cout<<"forward speed: "<<twist_msg.linear.x<<endl;
+                    ++k;
+              
                     my_rate.sleep();
+                } else {
+                    twist_msg.angular.z = 0;
+                    twist_msg.linear.x = 0;
+                    vel_pub.publish(twist_msg);
                 }
+                
             }
             twist_msg.angular.z = 0;
             twist_msg.linear.x = 0;
             vel_pub.publish(twist_msg);
-            /**
-            double desired_yaw = caculateDesiredOrientation(robotPosX, robotPosY(), waypoint, clockwise);
-            if(waypoint.y - robotPosY() == 0){
-                    desired_yaw = (waypoint.x - robotPosX) > 0 ? 0.0 : PI;
-            }
             
-            double current_yaw = quaternionToEuler().z;
-            double integral_yaw = 0.0, derivative_yaw = 0.0, lastError_yaw = 0.0;
-            double integral_dist = 0.0, derivative_dist = 0.0, lastError_dist = 0.0;
-            double Kp = 0.2, Kp_yaw = 0.5;
-            double Ki = 0.01;
-            double Kd = 0.06, Kd_yaw = 0.06;
-            
-            cout<< "robotPosX():"<<robotPosX()<<endl;
-            cout<< "robotPosY():"<<robotPosY()<<endl;
-            cout<<"current_yaw:"<<current_yaw<<endl;
-            cout<<"desired_yaw:"<<desired_yaw<<endl;
-
-            Rate rate(100);
-            geometry_msgs::Twist twist_msg;
-
-            while((getEuclideanDistance(robotPosX(), robotPosY(), waypoint) > tolerance) || (desired_yaw-current_yaw > yaw_tolerance)){
-                robotPosX() = odom_pose.pose.pose.position.x;
-                robotPosY() = odom_pose.pose.pose.position.y;
-                
-                //cout<<boolalpha<<"clockwise:"<<clockwise<<endl;
-
-                desired_yaw = caculateDesiredOrientation(robotPosX(), robotPosY(), waypoint, clockwise);
-                //cout<<"desired_yaw_inside:"<<desired_yaw<<endl;
-                
-                current_yaw = quaternionToEuler().z;
-                // if(current_yaw < 0 && !clockwise){
-                //     current_yaw = PI + (PI - fabs(current_yaw));
-                // }
-                // if(current_yaw >0 && clockwise){
-                //     current_yaw = -1 * (PI + (PI - fabs(current_yaw)));
-                // }
-                cout<<"current_yaw_inside:"<<current_yaw<<endl;
-                cout<<"desired_yaw-current_inside:"<<desired_yaw-current_yaw<<endl;
-
-                //PD control for velocity
-                double distance_error = getEuclideanDistance(robotPosX(),robotPosY(), waypoint);
-                integral_dist = integral_dist + distance_error;
-                derivative_dist = distance_error - lastError_dist;
-                lastError_dist = distance_error;
-                // double forward_speed = Kp*distance_error + Ki*integral_dist + Kd*derivative_dist;
-                double forward_speed = Kp*distance_error + Kd*derivative_dist;
-
-                // PD control for steering;
-                double error_yaw = desired_yaw - current_yaw;
-                integral_yaw = integral_yaw + error_yaw;
-                derivative_yaw = error_yaw - lastError_yaw;
-                lastError_yaw = error_yaw;
-                // double turn = Kp*error_yaw + Ki*integral_yaw + Kd*derivative_yaw;
-                double turn = Kp_yaw*error_yaw + Kd_yaw*derivative_yaw;
-
-                
-
-                twist_msg.linear.x = forward_speed;
-                twist_msg.linear.y = 0;
-                twist_msg.linear.z = 0;
-
-                twist_msg.angular.x = 0;
-                twist_msg.angular.y = 0;
-                twist_msg.angular.z = turn;
-                vel_pub.publish(twist_msg);
-                spinOnce();
-                rate.sleep();
-            }
-            twist_msg.linear.x = 0;
-            twist_msg.angular.z = 0;
-            vel_pub.publish(twist_msg);
-            */
         }
         void adaptive_pursuit(){
             // do adaptive pursuit
