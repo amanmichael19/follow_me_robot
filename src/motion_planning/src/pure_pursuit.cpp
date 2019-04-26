3f81a737bd3d927798484f8e1b6a1475d16c38bd
tesfami1 <tesfami1@msu.edu>
2019-04-26 02:00:54 -0400

    final touches update readme

--- src/motion_planning/src/pure_pursuit.cpp
+++ src/motion_planning/src/pure_pursuit.cpp
@@ -15,19 +15,51 @@
 
 using namespace std;
 using namespace ros;
-
+/**
+Point object to represent cartesian points.
+*/
 struct Point {
     double x;
     double y;
     Point(): x(0.0), y(0.0){}
     Point(double x_in, double y_in): x(x_in), y(y_in){}
 };
-
+/**
+Waypoint object representing path points.
+*/
 struct WayPoint : public Point {
     double curvature;
     bool visited;
     WayPoint(double x_in, double y_in, double curv_in = 0.0): Point(x_in, y_in), curvature(curv_in), visited(false){}
 };
+
+/**
+PIDcontroller object with method to maintain following distance
+*/
+struct PIDcontroller {
+  private:
+     double _Kp;
+     double _Ki;
+     double _Kd;
+     double _dt;
+     double _integral = 0;
+     double _derivative;
+     double _last_error = 0;
+     double _error;
+  public:
+     PIDcontroller(double Kp, double Ki, double Kd, double dt): _Kp(Kp), _Ki(Ki), _Kd(Kd), _dt(dt){}
+     double maintainFollowingDistance(double expected, double current){
+        _error = -1* (expected - current);
+        _integral += _error * _dt;
+        _derivative = (_error - _last_error) / _dt;
+        _last_error = _error;
+        return _Kp * _error + _Ki * _integral + _Kd * _derivative;
+     }
+};
+
+/**
+PurePursuit object
+*/
 
 class PurePursuit{
     private:
@@ -43,7 +75,8 @@ class PurePursuit{
       nav_msgs::Path _path_to_publish;
       int* _last_visited_waypt_idx = nullptr;
       int* _next_waypt_idx = nullptr;
-      bool no_run = false;
+      bool _no_run = false;
+      double _following_dist = 0.7;
 
 
     public:
@@ -57,6 +90,9 @@ class PurePursuit{
             runPursuit();
         }
 
+        /**
+        callbacks
+        */
         void pathCallback(const nav_msgs::Path::ConstPtr& path_msg){
             _generated_path = *path_msg;
         }
@@ -65,8 +101,11 @@ class PurePursuit{
             _odom_input_data = *odom_msg;
         }
 
+        /**
+        * function where the pursuit is run from
+        */
         void runPursuit(){
-            if(!no_run){
+            if(!_no_run){
                 basic_pursuit();
             } else {
                 ROS_ERROR("[ERROR]: can't start. there is error");
@@ -80,16 +119,6 @@ class PurePursuit{
         *
         */
 
-
-        // using figure8 to test the path tracking algorithm
-        // void formFigure8Path(){
-        //     double x = 0.0, y=0.0;
-        //     for(double i = 0.0; i<6.3; i+=0.314){ //6.911 0.628
-        //         x = 2*sin(i);
-        //         y = 2*sin(i)*cos(i);
-        //         _figure8_path.push_back(new WayPoint(x, y, 0.0));
-        //     }
-        // }
         //changing to waypoint object since every thing was done using that object at first so
         // instead of changing everything, I have this function to change from PoseStamped object
         // to WayPoint object
@@ -104,7 +133,7 @@ class PurePursuit{
                     _figure8_path.push_back(new WayPoint(path_point.pose.position.x, path_point.pose.position.y, path_point.pose.position.z));
                 }
             } else {
-                no_run = true;
+                _no_run = true;
                 ROS_ERROR("[ERROR]: No generated path");
                 return;
             }
@@ -163,6 +192,10 @@ class PurePursuit{
             double dist = numerator / sqrt(1 + pow(slope, 2));
             return dist;
         }
+        /**
+        * given a point q in a plane gets a point p on a line l so that the line
+        * qp is perpendicular to l
+        */
         template<typename T>
         Point* getPtOnLinePerpendicularToAPt(double slope, WayPoint* waypt, T* pt){
             double slope_2;
@@ -180,6 +213,9 @@ class PurePursuit{
             }
             return new Point(intersection_x, intersection_y);
         }
+        /**
+        checks is a point is on a line between two other points on that line
+        */
         template <typename T>
         bool betweenTwoptsOnALine(WayPoint* a, WayPoint* b, T* pt){
             double a_pt_dist = getEuclideanDistance(a->x, a->y, pt->x, pt->y);
@@ -191,6 +227,9 @@ class PurePursuit{
                 return false;
             }
         }
+        /**
+        gets intersection point between a circle and a line
+        */
         Point getIntersectionPtCircleLine(double slp, WayPoint* waypt_i, WayPoint* waypt_next, geometry_msgs::Point ctr){
             //cout<<"slp: "<< slp<<endl;
 
@@ -222,7 +261,9 @@ class PurePursuit{
             }
 
         }
-
+        /**
+        gets relative coordinates
+        */
         geometry_msgs::Point getRelativeCoordinate(geometry_msgs::Point point_msg, geometry_msgs::Pose current_pose){
             tf::Transform inverse;
             tf::poseMsgToTF(current_pose, inverse);
@@ -244,7 +285,9 @@ class PurePursuit{
         *
         */
 
-
+        /**
+        * checks is path has finished
+        */
         bool pathFinished(){
             WayPoint*& last_waypoint = _figure8_path.back();
             bool waypt_before_last_visited = _figure8_path[_figure8_path.size()-2]->visited;
@@ -300,9 +343,7 @@ class PurePursuit{
             }
         }
 
-        // void publishNavPath(WayPoint* waypt){
 
-        // }
 
         void setLastVisitedWaypt(){
             geometry_msgs::Point current_pose = currentRobotPose().position;
@@ -418,6 +459,7 @@ class PurePursuit{
             twist_msg.angular.x = 0;
             twist_msg.angular.y = 0;
 
+            PIDcontroller pid(0.2, 0.5, 0.01, 1/15);
             int k = 0;
             spinOnce();
 
@@ -425,14 +467,22 @@ class PurePursuit{
 
             while(!pathFinished()){
 
-            //while(!finished){
-                cout<<"**** "<< k <<" ****"<<endl;
                 bool is_on_path = onPath();
+                // Since we couldn't get Visp to work on TurtleBot camera, there is not target being
+                //tracked, hence we can't say target's positon. But use the line below to maintain following
+                //distance with target if that Visp works on the robot
+
+                // double dist_from_target = getEuclideanDistance();
+                // double forward_speed_increment = pid.maintainFollowingDistance(_following_dist, dist_from_target);
+                // forward_speed += forward_speed_increment;
+
+                cout<<"**** "<< k <<" ****"<<endl;
                 cout<<boolalpha<<"pathfinished: "<<pathFinished()<<endl;
                 cout<<boolalpha<<"is_on_path: "<<is_on_path<<endl;
                 cout<<"    robot_pose: "<<robotPosX()<<","<<robotPosY()<<endl;
+
+
                 if(is_on_path){
-                //if(1){
 
                     WayPoint* goalpt = getGoalptLookAheadDistanceAway(is_on_path);
                     if(isnan(goalpt->x) || isnan(goalpt->y) || goalpt == nullptr){
@@ -477,10 +527,6 @@ class PurePursuit{
             twist_msg.linear.x = 0;
             _vel_pub.publish(twist_msg);
 
-        }
-
-        void adaptive_pursuit(){
-            // do adaptive pursuit
         }
 };
 
